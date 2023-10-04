0e851fa0f071df9bdd13e2d715c9c0ea3699ef8d
ychrysostomakis <I.Chrysostomakis@leibniz-lib.de>
2023-10-04 19:19:37 +0200

    edit run file

--- SGE_annot.sh
+++ SGE_annot.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-#$ -S /bin/bash
-#$ -cwd
-#$ -j n
-#$ -q medium.q,large.q
-#$ -N genofish_HAP1
-#$ -pe smp 30
-#$ -M I.Chrysostomakis@leibniz-lib.de
-#$ -m beas
 
-# T.Oriowo@leibniz-lib.de
+# replace with own modules
+module load singularity
+module load anaconda3
+conda activate genome_annotation
 
-module load singularity/3.10.5_fix 
-module load anaconda3/2022.05
-conda activate genofish
-
-#one core will be used by snakemake to monitore the other processes
-THREADS=$(expr ${NSLOTS} - 1)
+THREADS=50
 
 snakemake -n \
     --snakefile workflow/Snakefile.smk \
@@ -27,11 +17,12 @@ snakemake -n \
     --singularity-args "--home $PWD" \
     --singularity-args "--bind $PWD/temp:/tmp" \
     -j ${THREADS} \
-    --default-resources "tmpdir='/share/pool/genofish_and_chips/testannotation/hap1_temp'" \
+    --default-resources "tmpdir='/path/to/tempdir'" \
     --verbose \
     --printshellcmds \
     --reason \
     --nolock \
     --rerun-triggers mtime \
-    --stats genofish_HAP1-stats_braker3maker2.txt
+    --stats "./stats.json" \
+    --report "./report.html"
 
